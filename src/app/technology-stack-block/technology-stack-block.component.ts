d56daceb81994f23727ad7baab467107802a112e
Carlos Alfonso Rivera Maldonado <carlosa.riveram@gmail.com>
2024-06-07 23:58:47 -0400

    Updated html to components

--- src/app/technology-stack-block/technology-stack-block.component.ts
+++ src/app/technology-stack-block/technology-stack-block.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { TechnologyStackDetail } from './technology-stack-detail';
 
 @Component({
   selector: 'app-technology-stack-block',
@@ -6,5 +7,44 @@ import { Component } from '@angular/core';
   styleUrls: ['./technology-stack-block.component.css']
 })
 export class TechnologyStackBlockComponent {
+  technologyStack: TechnologyStackDetail[];
 
+  constructor() {
+    this.technologyStack = this.getTechnologyStackDetail();
+    console.log(this.technologyStack);
+  }
+
+  getTechnologyStackDetail(): TechnologyStackDetail[] {
+    return [{
+      image: 'csharp.png',
+      alt: 'C#'
+    }, {
+      image: 'js.png',
+      alt: "JavaScript"
+    }, {
+      image: 'php.png',
+      alt: "PHP"
+    }, {
+      image: 'sql-server.png',
+      alt: "SQL Server"
+    }, {
+      image: 'mysql.png',
+      alt: "MySQL"
+    }, {
+      image: 'postgresql.png',
+      alt: "PostgreSQL"
+    }, {
+      image: 'elastic.png',
+      alt: "Elastic"
+    }, {
+      image: 'aws.png',
+      alt: "AWS"
+    }, {
+      image: 'azure.png',
+      alt: "Azure"
+    }, {
+      image: 'google-cloud.svg',
+      alt: "Google Cloud"
+    }]
+  }
 }
---
+++ src/app/technology-stack-block/technology-stack-detail.ts
@@ -0,0 +1,4 @@
+export interface TechnologyStackDetail {
+    image: string;
+    alt: string;
+}
